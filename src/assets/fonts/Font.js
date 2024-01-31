748d51df6a7ce60dda9eb02c24422ca2254e0139
Ajay Soliya <ranktrushal15@gmail.com>
2024-01-31 16:56:22 +0530

    changed from react native cli to expo.

---
+++ src/assets/fonts/Font.js
@@ -0,0 +1,8 @@
+export const Font = {
+    DancingScript: "DancingScript-Bold",
+    EBGaramond: "EBGaramond",
+    Inconsolata_Condensed: "Inconsolata_Condensed",
+    Inconsolata_ExtraCondensed: "Inconsolata_ExtraCondensed",
+    Lora: "Lora",
+    Playfair_Display: "Playfair_Display"
+} 
---
+++ src/common/color.ts
@@ -0,0 +1,10 @@
+export const COLOR = {
+    infinity: '#212830',
+    white: '#ffffff',
+    electricBlue: '#7df0ff',
+    blue: '#0080ff',
+    grey: 'grey',
+    black: "black",
+    lightGrey: '#e5e5e5e6',
+    orinet: '#205e78',
+}
